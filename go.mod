1bbb9e72a1a6b0f2451a99a7f78b79a886617b17
MrFuku <mmafighter_shun@icloud.com>
2020-04-22 20:54:02 +0900

    とりあえずDynamo接続できたがやっぱ使わない
    # ======== Emoji Prefix ========
    # 🎉  :tada: 初回commit用. なにか記念すべきcommitならつける
    # ✨  :sparkles: 新しい機能のcommitならつける. あくまでも機能の追加
    # 🔧  :wrench: 設定ファイル系の修正commitにつける.
    # 💄  :lipstick: UIや見た目に関わる修正・更新commitにつける.
    # ✏  :pencil2: タイポの修正commitにつける.
    # 📝  :memo: コメントやテキストドキュメントの更新commitにつける.
    # 🔥  :fire: コードの削除、ファイルの削除commitにつける.
    # 🐛  :bug: バグの修正commitにつける.
    # ♻  :recycle: コードの整形、リファクタcommitにつける.
    # 🚨  :rotating_light: Rubocopなどのlinterの警告対応commitにつける.
    # 🚧  :construction: お試しcommitに対してつける.

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/MrFuku/shogi-backend
 go 1.14
 
 require (
+	github.com/aws/aws-sdk-go v1.30.11
+	github.com/guregu/dynamo v1.7.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20200414173820-0848c9571904 // indirect
--- main.go
+++ main.go
@@ -2,16 +2,53 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"os"
 
-	"github.com/labstack/echo"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/guregu/dynamo"
 )
 
+type Sample struct {
+	UserID string `dynamo:"UserID,hash"`
+	Name   string `dynamo:"Name"`
+}
+
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("ap-northeast-1"),
+		Endpoint:    aws.String(os.Getenv("DYNAMO_ENDPOINT")),
+		Credentials: credentials.NewStaticCredentials("dummy", "dummy", "dummy"),
 	})
-	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
+	if err != nil {
+		panic(err)
+	}
+
+	db := dynamo.New(sess)
+
+	// err = db.CreateTable("Samples", Sample{}).Run()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	table := db.Table("Samples")
+	err = table.Put(&Sample{UserID: "1", Name: "Test1"}).Run()
+	if err != nil {
+		panic(err)
+	}
+
+	var sample Sample
+	err = table.Get("UserID", "1").One(&sample)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", sample)
+
+	// e := echo.New()
+	// e.GET("/", func(c echo.Context) error {
+	// 	return c.String(http.StatusOK, "Hello, World!")
+	// })
+	// e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
 }
