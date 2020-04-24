5f14d4a30ebdef82081af1777805ac042e0be2e8
MrFuku <mmafighter_shun@icloud.com>
2020-04-24 22:40:20 +0900

    router pkg を定義

--- pkg/interfaces/api/server/router/router.go
+++ pkg/interfaces/api/server/router/router.go
@@ -1,11 +1,11 @@
-package main
+package router
 
 import (
 	"fmt"
 	"net/http"
 )
 
-func routeSetting() {
+func RouteSetting() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	})
