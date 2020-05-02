e617e4667ed82963d827775b6144737b223c2857
MrFuku <mmafighter_shun@icloud.com>
2020-05-02 20:58:44 +0900

    プリフライトリクエストを許可

--- pkg/interfaces/api/server/router/router.go
+++ pkg/interfaces/api/server/router/router.go
@@ -22,6 +22,11 @@ func wrapHandler(fn http.HandlerFunc) http.HandlerFunc {
 func withCORS(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
 		fn(w, r)
 	}
 }
